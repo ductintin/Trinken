901093113f75b0a92c2823baae239ea30c411439
ductintin <buiductienpy206@gmail.com>
2023-03-25 13:22:31 +0700

    1

---
+++ app/src/main/java/vn/tp/trinken/Activity/HomeActivity.java
@@ -0,0 +1,14 @@
+package vn.tp.trinken.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import vn.tp.trinken.R;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
