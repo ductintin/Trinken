7dc9430c452ca61d9da05acd730b54f963c62248
ductintin <buiductienpy206@gmail.com>
2023-03-30 15:36:26 +0700

    Welcome page

---
+++ app/src/main/java/vn/tp/trinken/Activity/WelcomeActivity.java
@@ -0,0 +1,112 @@
+package vn.tp.trinken.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import vn.tp.trinken.R;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import vn.tp.trinken.Model.*;
+import vn.tp.trinken.Adapter.*;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private LinearLayout dotsLL;
+    SliderAdapter adapter;
+    private ArrayList<SliderModal> sliderModalArrayList;
+    private TextView[] dots;
+    int size;
+
+    Button btnSkip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        // initializing all our views.
+        viewPager = findViewById(R.id.idViewPager);
+        dotsLL = findViewById(R.id.idLLDots);
+
+        // in below line we are creating a new array list.
+        sliderModalArrayList = new ArrayList<>();
+
+        // on below 3 lines we are adding data to our array list.
+        sliderModalArrayList.add(new SliderModal("Trinken", "DISCOVERY", "@drawable/courier",  "YOUR FAVORITE DRINK"));
+        sliderModalArrayList.add(new SliderModal("Trinken", "CHOOSE", "https://images.unsplash.com/photo-1610783131813-475d08664ef6?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",  "HE DRINK YOU LOVE"));
+        sliderModalArrayList.add(new SliderModal("Trinken", "DELIVERY", "https://images.unsplash.com/photo-1610832958506-aa56368176cf?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60","ENJOY THE DRINK"));
+
+        // below line is use to add our array list to adapter class.
+        adapter = new SliderAdapter(WelcomeActivity.this, sliderModalArrayList);
+
+        // below line is use to set our
+        // adapter to our view pager.
+        viewPager.setAdapter(adapter);
+
+        // we are storing the size of our
+        // array list in a variable.
+        size = sliderModalArrayList.size();
+
+        // calling method to add dots indicator
+        addDots(size, 0);
+
+        // below line is use to call on
+        // page change listener method.
+        viewPager.addOnPageChangeListener(viewListener);
+    }
+
+    private void addDots(int size, int pos) {
+        // inside this method we are
+        // creating a new text view.
+        dots = new TextView[size];
+
+        // below line is use to remove all
+        // the views from the linear layout.
+        dotsLL.removeAllViews();
+
+        // running a for loop to add
+        // number of dots to our slider.
+        for (int i = 0; i < size; i++) {
+            // below line is use to add the
+            // dots and modify its color.
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("."));
+            dots[i].setTextSize(35);
+
+            // below line is called when the dots are not selected.
+            dots[i].setTextColor(getResources().getColor(R.color.black));
+            dotsLL.addView(dots[i]);
+        }
+        if (dots.length > 0) {
+            // this line is called when the dots
+            // inside linear layout are selected
+            dots[pos].setTextColor(getResources().getColor(R.color.purple_200));
+        }
+    }
+
+    // creating a method for view pager for on page change listener.
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            // we are calling our dots method to
+            // change the position of selected dots.
+            addDots(size, position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+}
---
+++ app/src/main/java/vn/tp/trinken/Adapter/SliderAdapter.java
@@ -0,0 +1,96 @@
+package vn.tp.trinken.Adapter;
+
+import vn.tp.trinken.R;
+import vn.tp.trinken.Model.*;
+import android.content.Context;
+import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class SliderAdapter extends PagerAdapter {
+
+    // creating variables for layout
+    // inflater, context and array list.
+    LayoutInflater layoutInflater;
+    Context context;
+    ArrayList<SliderModal> sliderModalArrayList;
+
+    // creating constructor.
+    public SliderAdapter(Context context, ArrayList<SliderModal> sliderModalArrayList) {
+        this.context = context;
+        this.sliderModalArrayList = sliderModalArrayList;
+    }
+
+    @Override
+    public int getCount() {
+        // inside get count method returning
+        // the size of our array list.
+        return sliderModalArrayList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        // inside isViewFromobject method we are
+        // returning our Relative layout object.
+        return view == (ConstraintLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        // in this method we will initialize all our layout
+        // items and inflate our layout file as well.
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+
+        // below line is use to inflate the
+        // layout file which we created.
+        View view = layoutInflater.inflate(R.layout.welcome_layout1, container, false);
+
+        // initializing our views.
+        ImageView imageView = view.findViewById(R.id.image);
+        TextView appName = view.findViewById(R.id.tvAppName);
+        TextView title = view.findViewById(R.id.tvTitle);
+        TextView subTitle = view.findViewById(R.id.tvSubTitle);
+        ConstraintLayout sliderRL = view.findViewById(R.id.layout);
+
+        // setting data to our views.
+        SliderModal modal = sliderModalArrayList.get(position);
+        appName.setText(modal.getTitle());
+        title.setText(modal.getHeading());
+        subTitle.setText(modal.getSubTitle());
+        Picasso.get().load(modal.getImgUrl()).into(imageView);
+
+        // below line is to set background
+        // drawable to our each item
+        //sliderRL.setBackground(context.getResources().getDrawable(modal.getBackgroundDrawable()));
+        //sliderRL.setBackground(ContextCompat.getDrawable(context, modal.getBackgroundDrawable()));
+        // after setting the data to our views we
+        // are adding the view to our container.
+        container.addView(view);
+
+        // at last we are
+        // returning the view.
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        // this is a destroy view method
+        // which is use to remove a view.
+        container.removeView((ConstraintLayout) object);
+    }
+}
