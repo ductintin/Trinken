2dee051bc2ebfe051763b6bf203a11b24ac78eb9
Phát Nguyễn <moonwolfjoker@gmail.com>
2023-04-22 11:14:16 +0700

    login

--- app/src/main/java/vn/tp/trinken/Model/User.java
+++ app/src/main/java/vn/tp/trinken/Model/User.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Users implements Serializable {
+public class User implements Serializable {
     @SerializedName("id")
     private int user_id;
 
