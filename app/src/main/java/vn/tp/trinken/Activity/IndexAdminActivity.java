c3fe8a459415c6ab7f859bc9df8a8a6c089918e7
ductintin <buiductienpy206@gmail.com>
2023-05-12 00:01:29 +0700

    delete cartItem

---
+++ app/src/main/java/vn/tp/trinken/Activity/IndexAdminActivity.java
@@ -0,0 +1,14 @@
+package vn.tp.trinken.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IndexAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index_admin);
+    }
+}
